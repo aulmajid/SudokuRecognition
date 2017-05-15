d1ec3c68937744c44c50d71c79c81f9ead82618a
Majid <aulmajid@gmail.com>
2017-05-15 23:05:29 +0700

    sudah bisa ambil gambar sudokunya

--- sudoku.py
+++ sudoku.py
@@ -1,82 +1,17 @@
 import cv2
 import numpy as np
-import math
+import utils
 
-
-def draw_line(img, line, rgb):
-    if line[1] <> 0:
-        m = -1 / np.tan(line[1])
-        c = line[0] / np.sin(line[1])
-        cv2.line(img, (0, c), (img.shape[1], int(m * img.shape[1] + c)), rgb)
-    else:
-        cv2.line(img, (line[0], 0), (line[0], img.shape[0]), rgb)
-
-
-def draw_lines(img, lines, rgb):
-    for line in lines:
-        line = line[0]
-        draw_line(outer_box, line, rgb)
-
-
-def merge_related_lines(img, lines):
-    for line1 in lines:
-        line1 = line1[0]
-        if line1[0] == 0 and line1[1] == -100:
-            continue
-        rho1 = line1[0]
-        theta1 = line1[1]
-        if theta1 > np.pi * 45 / 180 and theta1 < np.pi * 135 / 180:
-            pt11x = 0
-            pt11y = rho1 / np.sin(theta1)
-            pt12x = img.shape[1]
-            pt12y = -pt12x / np.tan(theta1) + rho1 / np.sin(theta1)
-        else:
-            pt11x = rho1 / np.cos(theta1)
-            pt11y = 0
-            pt12y = img.shape[0]
-            pt12x = -pt12y / np.tan(theta1) + rho1 / np.cos(theta1)
-
-        for line2 in lines:
-            line2 = line2[0]
-            if (line1 == line2).all():
-                continue
-            rho2 = line2[0]
-            theta2 = line2[1]
-            if np.fabs(rho2 - rho1) < 20 and np.fabs(theta2 - theta1) < np.pi * 10 / 180:
-                if theta2 > np.pi * 45 / 180 and theta2 < np.pi * 135 / 180:
-                    pt21x = 0
-                    pt21y = rho2 / np.sin(theta2)
-                    pt22x = img.shape[1]
-                    pt22y = -pt22x / np.tan(theta2) + rho2 / np.sin(theta2)
-                else:
-                    pt21x = rho2 / np.cos(theta2)
-                    pt21y = 0
-                    pt22y = img.shape[0]
-                    pt22x = -pt22y / np.tan(theta1) + rho2 / np.cos(theta2)
-
-                dst1x = pt21x - pt11x
-                dst1y = pt21y - pt11y
-                dst2x = pt22x - pt12x
-                dst2y = pt22y - pt12y
-                if dst1x * dst1x + dst1y * dst1y < 64 * 64 and dst2x * dst2x + dst2y + dst2y < 64 * 64:
-                    line1[0] = (line1[0] + line2[0]) / 2
-                    line1[1] = (line1[1] + line2[1]) / 2
-                    line2[0] = 0
-                    line2[1] = -100
-
-
-path = 'citra/sudoku1.jpg'
+path = 'citra/sudoku.jpg'
 
 # image
 sudoku = cv2.imread(path)
-sudoku = cv2.cvtColor(sudoku, cv2.COLOR_BGR2GRAY)
-
-# preprocessing
-sudoku = cv2.GaussianBlur(sudoku, (11, 11), 0)
-outer_box = cv2.adaptiveThreshold(sudoku, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 5, 2)
-outer_box = cv2.bitwise_not(outer_box)
 
 # cari kotak terluar
+outer_box = cv2.cvtColor(sudoku, cv2.COLOR_BGR2GRAY)
+outer_box = cv2.GaussianBlur(outer_box, (11, 11), 0)
+outer_box = cv2.adaptiveThreshold(outer_box, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 5, 2)
+outer_box = cv2.bitwise_not(outer_box)
 kernel = np.array([[0., 1., 0.], [1., 1., 1.], [0., 1., 0.]])
 outer_box = cv2.dilate(outer_box, kernel)
 count = 0
@@ -98,12 +33,29 @@ for i in range(w):
             outer_box[j][i] = 0
 outer_box = cv2.erode(outer_box, kernel)
 
-# hough transform dan gabung line-line yang berdekatan
+# cari garis2 dengan hough transform
 lines = cv2.HoughLines(outer_box, 1, np.pi / 180, 200)
-merge_related_lines(sudoku, lines)
-outer_box = cv2.cvtColor(outer_box, cv2.COLOR_GRAY2BGR)
-draw_lines(outer_box, lines, (0, 0, 128))
+lines = utils.mergeRelatedLines(sudoku, lines)
+
+# cari garis2 terluar
+top, bottom, left, right = utils.findExtremeLines(lines)
+extremes = [[top], [bottom], [left], [right]]
+
+# warping
+src = np.zeros((4, 2), "float32")
+src[0] = utils.getLineIntersect(sudoku, top, left)
+src[1] = utils.getLineIntersect(sudoku, top, right)
+src[2] = utils.getLineIntersect(sudoku, bottom, right)
+src[3] = utils.getLineIntersect(sudoku, bottom, left)
+newSize = 400
+dst = np.array([
+    [0, 0],
+    [newSize, 0],
+    [newSize, newSize],
+    [0, newSize]
+], "float32")
+M = cv2.getPerspectiveTransform(src, dst)
+sudoku = cv2.warpPerspective(sudoku, M, (newSize, newSize))
 
 cv2.imshow('sudoku', sudoku)
-cv2.imshow('outer', outer_box)
 cv2.waitKey()
---
+++ utils.py
@@ -0,0 +1,128 @@
+import cv2
+import numpy as np
+
+def convertToXY(img, line):
+    if line[1] <> 0:
+        m = -1 / np.tan(line[1])
+        c = line[0] / np.sin(line[1])
+        pt1x = 0
+        pt1y = c
+        pt2x = img.shape[1]
+        pt2y = int(m * img.shape[1] + c)
+    else:
+        pt1x = line[0]
+        pt1y = 0
+        pt2x = line[0]
+        pt2y = img.shape[0]
+    return (pt1x, pt1y), (pt2x, pt2y)
+
+
+def drawLine(img, line, rgb):
+    pt1, pt2 = convertToXY(img, line)
+    cv2.line(img, pt1, pt2, rgb)
+
+
+def drawLines(img, lines, rgb):
+    for line in lines:
+        line = line[0]
+        drawLine(img, line, rgb)
+
+
+def mergeRelatedLines(img, lines):
+    temp = lines.copy()
+    for line1 in temp:
+        line1 = line1[0]
+        if line1[0] == 0 and line1[1] == -100:
+            continue
+        r1 = line1[0]
+        theta1 = line1[1]
+        if theta1 > np.pi * 45 / 180 and theta1 < np.pi * 135 / 180:
+            pt11x = 0
+            pt11y = r1 / np.sin(theta1)
+            pt12x = img.shape[1]
+            pt12y = -pt12x / np.tan(theta1) + r1 / np.sin(theta1)
+        else:
+            pt11x = r1 / np.cos(theta1)
+            pt11y = 0
+            pt12y = img.shape[0]
+            pt12x = -pt12y / np.tan(theta1) + r1 / np.cos(theta1)
+
+        for line2 in temp:
+            line2 = line2[0]
+            if (line1 == line2).all() or (line2[0] == 0 and line2[1] == -100):
+                continue
+            r2 = line2[0]
+            theta2 = line2[1]
+            if np.fabs(r2 - r1) < 20 and np.fabs(theta2 - theta1) < np.pi * 10 / 180:
+                if theta2 > np.pi * 45 / 180 and theta2 < np.pi * 135 / 180:
+                    pt21x = 0
+                    pt21y = r2 / np.sin(theta2)
+                    pt22x = img.shape[1]
+                    pt22y = -pt22x / np.tan(theta2) + r2 / np.sin(theta2)
+                else:
+                    pt21x = r2 / np.cos(theta2)
+                    pt21y = 0
+                    pt22y = img.shape[0]
+                    pt22x = -pt22y / np.tan(theta1) + r2 / np.cos(theta2)
+
+                dst1x = pt21x - pt11x
+                dst1y = pt21y - pt11y
+                dst2x = pt22x - pt12x
+                dst2y = pt22y - pt12y
+                if dst1x * dst1x + dst1y * dst1y < 64 * 64 and dst2x * dst2x + dst2y + dst2y < 64 * 64:
+                    line1[0] = (line1[0] + line2[0]) / 2
+                    line1[1] = (line1[1] + line2[1]) / 2
+                    line2[0] = 0
+                    line2[1] = -100
+
+    delete_index = []
+    for i in range(len(temp)):
+        line = temp[i][0]
+        if line[0] == 0 and line[1] == -100:
+            delete_index.append(i)
+    temp = np.delete(temp, delete_index, 0)
+    return temp
+
+
+def findExtremeLines(lines):
+    topLine = (1000, 1000)
+    bottomLine = (-1000, -1000)
+    leftXIntercept = 100000
+    rightXIntercept = 0
+
+    for line in lines:
+        line = line[0]
+        r = line[0]
+        theta = line[1]
+        xIntercept = r / np.cos(theta)
+
+        if theta > np.pi * 80 / 180 and theta < np.pi * 100 / 180:
+            if r < topLine[0]:
+                topLine = line
+            if r > bottomLine[0]:
+                bottomLine = line
+        elif theta < np.pi * 10 / 180 or theta > np.pi * 170 / 180:
+            if xIntercept > rightXIntercept:
+                rightLine = line
+                rightXIntercept = xIntercept
+            elif xIntercept <= leftXIntercept:
+                leftLine = line
+                leftXIntercept = xIntercept
+
+    return topLine, bottomLine, leftLine, rightLine
+
+
+def det(a, b):
+    return a[0] * b[1] - a[1] * b[0]
+
+
+def getLineIntersect(img, line1, line2):
+    line1 = convertToXY(img, line1)
+    line2 = convertToXY(img, line2)
+    xdiff = (line1[0][0] - line1[1][0], line2[0][0] - line2[1][0])
+    ydiff = (line1[0][1] - line1[1][1], line2[0][1] - line2[1][1])
+    div = det(xdiff, ydiff)
+    d = (det(line1[0], line1[1]), det(line2[0], line2[1]))
+    x = det(d, xdiff) / div
+    y = det(d, ydiff) / div
+    return int(x), int(y)
